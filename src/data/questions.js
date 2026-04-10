f91428275891007ca36d4c02205e5a27283e98ef
MartiMat1 <martimat@uef.fi>
2026-04-10 11:53:48 +0300

    Creating posts API

---
+++ src/data/questions.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    question: "How many strings are in normal acoustic guitar?",
+    answer: "5",
+    keywords: ["music","pubquiz"],
+  },
+  {
+    id: 2,
+    question: "What is the capital of France?",
+    answer: "Paris",
+    keywords: ["geography","pubquiz"],
+  },
+  {
+    id: 3,
+    question: "Who are you?",
+    answer: "I am me",
+    keywords: ["philosophy","pubquiz"],
+  },
+  {
+    id: 4,
+    question: "What",
+    answer: "?",
+    keywords: ["question","pubquiz"],
+  }
+];
+
+module.exports = questions;
